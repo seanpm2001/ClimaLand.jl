80bcbecea1866b24b11990cf67ef5696008ddbfd
kmdeck <kdeck@caltech.edu>
2023-10-19 14:43:13 -0700

    cleaned up plots

--- experiments/integrated/flux_helper_functions.jl
+++ experiments/integrated/flux_helper_functions.jl
@@ -18,7 +18,7 @@ Cv = 0.01 # CLM right after 2.5.122
 Cs_canopy = 0.004 # CLM 2.5.120
 
 # Define the functions that compute the surface fluxes.
-function atmos_airspace_fluxes(ts_sfc, ts_in, atmos_h, d_sfc, u_air,z_0m, z_0b, surface_flux_params)
+function atmos_airspace_fluxes(ts_sfc, ts_in, atmos_h, d_sfc, u_air,z_0m, z_0b, surface_flux_params,cp_m)
     state_sfc =
         SurfaceFluxes.SurfaceValues(FT(0), SVector{2, FT}(0, 0), ts_sfc)
     state_in =
@@ -36,15 +36,16 @@ function atmos_airspace_fluxes(ts_sfc, ts_in, atmos_h, d_sfc, u_air,z_0m, z_0b, 
             sc;
             tol_neutral = SFP.cp_d(surface_flux_params) / 10000,
         )
-    cp_d = Thermodynamics.Parameters.cp_d(thermo_params)
-    grav = SurfaceFluxes.Parameters.grav(surface_flux_params)
-    R_d = Thermodynamics.Parameters.R_d(thermo_params)
-    T_0 = LSMP.T_0(earth_param_set)
     T_sfc = Thermodynamics.air_temperature(thermo_params, ts_sfc)
-    hd_sfc = cp_d * (T_sfc - T_0) + R_d * T_0
-    E0 = SurfaceFluxes.evaporation(surface_flux_params, sc, conditions.Ch)
+    T_in = Thermodynamics.air_temperature(thermo_params, ts_in)
+    q_sfc = Thermodynamics.total_specific_humidity(thermo_params, ts_sfc)
+    q_in = Thermodynamics.total_specific_humidity(thermo_params, ts_in)
     r_a = 1 / (conditions.Ch * SurfaceFluxes.windspeed(sc)) # definition
-    return (r_a = r_a, ustar = conditions.ustar, shf = conditions.shf + hd_sfc*E0, lhf = conditions.lhf- hd_sfc*E0, evaporation = conditions.evaporation, L_MO = conditions.L_MO)
+    ρ_air = Thermodynamics.air_density(thermo_params, ts_in)
+    simple_shf = -ρ_air/r_a*cp_m*(T_in - T_sfc)
+    simple_e = -ρ_air/r_a* (q_in -q_sfc)
+    _LH_v0 = SurfaceFluxes.Parameters.LH_v0(surface_flux_params)
+    return (r_a = r_a, ustar = conditions.ustar, shf = simple_shf, lhf = simple_e*_LH_v0, evaporation = simple_e, L_MO = conditions.L_MO)
 end
 
 function airspace_canopy_fluxes(ustar, T_airspace, q_airspace, T_canopy, ρ_air, cp_m, LAI, AI, r_canopy)
@@ -70,18 +71,18 @@ function land_lw_fluxes(LW_d, T_canopy, T_soil, ϵ_soil, ϵ_canopy, _σ)
     return (;LW_canopy = LW_canopy, LW_out = LW_out, LW_soil = LW_soil)
 end
 
-function airspace_soil_fluxes(ustar, T_airspace, q_airspace, W, T_soil, q_soil, r_soil, ρ_air, cp_m, ΔΦ)
+function airspace_soil_fluxes(ustar, T_airspace, q_airspace, W, T_soil, q_soil, r_soil, ρ_air, cp_m)
     Cs_bare = 0.4/0.13*(0.01*ustar/(1.5e-5))^(-0.45) # CLM Equation 2.5.121
     Cs = @. W*Cs_bare + (1-W)*Cs_canopy # CLM Equation 2.5.118
     r_ah_cs = @. 1/Cs/ustar # CLm Eq. 2.5.116 and 2.5.117
     ΔT = (T_airspace - T_soil)
-    soil_shf = -ρ_air * (cp_m * ΔT + ΔΦ) / r_ah_cs
+    soil_shf = -ρ_air * (cp_m * ΔT) / r_ah_cs
     soil_evap = -ρ_air * (q_airspace - q_soil) / (r_ah_cs + r_soil)
     return (;shf = soil_shf, evaporation = soil_evap)
 end
 
 
-function atmos_sfc_fluxes(ts_sfc, ts_in, atmos_h, d_sfc, u_air,z_0m, z_0b, surface_flux_params, thermo_params, r_sfc)
+function atmos_sfc_fluxes(ts_sfc, ts_in, atmos_h, d_sfc, u_air,z_0m, z_0b, surface_flux_params, thermo_params, r_sfc, cp_m)
     state_sfc =
         SurfaceFluxes.SurfaceValues(FT(0), SVector{2, FT}(0, 0), ts_sfc)
     state_in =
@@ -99,16 +100,14 @@ function atmos_sfc_fluxes(ts_sfc, ts_in, atmos_h, d_sfc, u_air,z_0m, z_0b, surfa
         sc;
         tol_neutral = SFP.cp_d(surface_flux_params) / 10000,
     )
-    cp_d = Thermodynamics.Parameters.cp_d(thermo_params)
-    R_d = Thermodynamics.Parameters.R_d(thermo_params)
-    T_0 = LSMP.T_0(earth_param_set)
     T_sfc = Thermodynamics.air_temperature(thermo_params, ts_sfc)
-    hd_sfc = cp_d * (T_sfc - T_0) + R_d * T_0
-    E0 = SurfaceFluxes.evaporation(surface_flux_params, sc, conditions.Ch)
+    T_in = Thermodynamics.air_temperature(thermo_params, ts_in)
+    q_sfc = Thermodynamics.total_specific_humidity(thermo_params, ts_sfc)
+    q_in = Thermodynamics.total_specific_humidity(thermo_params, ts_in)
     r_a = 1 / (conditions.Ch * SurfaceFluxes.windspeed(sc)) # definition
-    E = E0 * r_a / (r_sfc + r_a)
-    H = conditions.shf + hd_sfc * E0
-    LH = conditions.lhf * r_a / (r_sfc + r_a) - hd_sfc*E
-
-    return (r_a = r_a, ustar = conditions.ustar, shf = H, lhf = LH, evaporation = E, L_MO = conditions.L_MO)
+    ρ_air = Thermodynamics.air_density(thermo_params, ts_in)
+    simple_shf = -ρ_air/r_a*cp_m*(T_in - T_sfc)
+    simple_e = -ρ_air/(r_a+ r_sfc)* (q_in -q_sfc)
+    _LH_v0 = SurfaceFluxes.Parameters.LH_v0(surface_flux_params)
+    return (r_a = r_a, ustar = conditions.ustar, shf = simple_shf, lhf = simple_e*_LH_v0, evaporation = simple_e, L_MO = conditions.L_MO)
 end
--- experiments/integrated/ozark/ozark_parallel_offline.jl
+++ experiments/integrated/ozark/ozark_parallel_offline.jl
@@ -10,6 +10,7 @@ using StaticArrays
 using Thermodynamics
 using SurfaceFluxes
 using NLsolve
+using CurveFit
 
 using ClimaLSM
 using ClimaLSM.Domains: Column
@@ -35,192 +36,7 @@ include(
     joinpath(climalsm_dir, "experiments/integrated/ozark/ozark_parameters.jl"),
 )
 include(joinpath(climalsm_dir, "experiments/integrated/flux_helper_functions.jl"))
-
-# Met/Soil/LAI data - get timeseries for dates of interest
-P_air = atmos.P.(seconds)
-q_air = atmos.q.(seconds)
-T_air = atmos.T.(seconds)
-u_air = atmos.u.(seconds)
-h = atmos_h
-LW_d = radiation.LW_d.(seconds)
-SW_d = radiation.SW_d.(seconds)
-T_soil = TS
-ϑ_soil = SWC
-LAI = LAIfunction.(seconds)
-ref_time = atmos.ref_time
-θs = radiation.θs.(seconds, Ref(radiation.orbital_data), ref_time)
-c_co2_air = atmos.c_co2.(seconds)
-
-
-# Constants and model parameters:
-_ρ_liq = LSMP.ρ_cloud_liq(earth_param_set)
-thermo_params = LSMP.thermodynamic_parameters(earth_param_set)
-_LH_v0 = LSMP.LH_v0(earth_param_set)
-surface_flux_params = LSMP.surface_fluxes_parameters(earth_param_set)
-c = FT(LSMP.light_speed(earth_param_set))
-planck_h = FT(LSMP.planck_constant(earth_param_set))
-N_a = FT(LSMP.avogadro_constant(earth_param_set))
-grav = FT(LSMP.grav(earth_param_set))
-R = FT(LSMP.gas_constant(earth_param_set))
-M_w = LSMP.molar_mass_water(earth_param_set)
-hcm = ClimaLSM.Soil.vanGenuchten(; α = soil_vg_α, n = soil_vg_n)
-# soil params
-soil_params = Soil.EnergyHydrologyParameters{FT}(;
-    κ_dry = κ_dry,
-    κ_sat_frozen = κ_sat_frozen,
-    κ_sat_unfrozen = κ_sat_unfrozen,
-    ρc_ds = ρc_ds,
-    ν = soil_ν,
-    ν_ss_om = ν_ss_om,
-    ν_ss_quartz = ν_ss_quartz,
-    ν_ss_gravel = ν_ss_gravel,
-    hydrology_cm = hcm,
-    K_sat = soil_K_sat,
-    S_s = soil_S_s,
-    θ_r = θ_r,
-    earth_param_set = earth_param_set,
-    z_0m = z_0m_soil,
-    z_0b = z_0b_soil,
-    emissivity = soil_ϵ,
-    PAR_albedo = soil_α_PAR,
-    NIR_albedo = soil_α_NIR,
-);
-
-
-# Canopy Parameterizations - Only radiative_transfer, Photosynthesis, and stomatal conductance needed here.
-radiative_transfer = Canopy.TwoStreamModel{FT}(TwoStreamParameters{FT}(;Ω = Ω,
-                                                                       ld = ld,
-                                                                       α_PAR_leaf = α_PAR_leaf,
-                                                                       λ_γ_PAR = λ_γ_PAR,
-                                                                       λ_γ_NIR = λ_γ_NIR,
-                                                                       τ_PAR_leaf = τ_PAR_leaf,
-                                                                       α_NIR_leaf = α_NIR_leaf,
-                                                                       τ_NIR_leaf = τ_NIR_leaf,
-                                                                       n_layers = n_layers,
-                                                                       ϵ_canopy = ϵ_canopy,
-                                                                       )
-                                               )
-conductance = Canopy.MedlynConductanceModel{FT}(MedlynConductanceParameters{FT}(;
-                                                                                g1 = g1,
-                                                                                Drel = Drel,
-                                                                                g0 = g0,
-                                                                                )
-                                                )
-
-photosynthesis = Canopy.FarquharModel{FT}(FarquharParameters{FT}(Canopy.C3();
-                                                                 oi = oi,
-                                                                 ϕ = ϕ,
-                                                                 θj = θj,
-                                                                 f = f,
-                                                                 sc = sc,
-                                                                 pc = pc,
-                                                                 Vcmax25 = Vcmax25,
-                                                                 Γstar25 = Γstar25,
-                                                                 Kc25 = Kc25,
-                                                                 Ko25 = Ko25,
-                                                                 To = To,
-                                                                 ΔHkc = ΔHkc,
-                                                                 ΔHko = ΔHko,
-                                                                 ΔHVcmax = ΔHVcmax,
-                                                                 ΔHΓstar = ΔHΓstar,
-                                                                 ΔHJmax = ΔHJmax,
-                                                                 ΔHRd = ΔHRd,
-                                                                 )
-                                          )
-
-energy_per_photon_PAR = planck_h * c / λ_γ_PAR
-energy_per_photon_NIR = planck_h * c / λ_γ_NIR
-
-# Compute other diagnostic variables as timeseries:
-K = @. extinction_coeff(ld, θs)
-PAR = ClimaLSM.Canopy.compute_PAR.(Ref(radiative_transfer), Ref(radiation), seconds)
-NIR = ClimaLSM.Canopy.compute_NIR.(Ref(radiative_transfer), Ref(radiation), seconds)
-e_sat =
-    Thermodynamics.saturation_vapor_pressure.(
-        Ref(thermo_params),
-        T_air,
-        Ref(Thermodynamics.Liquid()),
-    )
-e =
-    Thermodynamics.partial_pressure_vapor.(
-        Ref(thermo_params),
-        P_air,
-        PhasePartition.(q_air),
-    )
-rel_hum = e ./ e_sat
-DOY = @. Dates.dayofyear(ref_time + Dates.Second(floor(Int64, seconds)))
-frac_diff = @. ClimaLSM.Canopy.diffuse_fraction(DOY, T_air, PAR + NIR, rel_hum, θs)
-# Compute the absorbed, reflected, and transmitted PAR and NIR. All positive by defn, in moles of photons/m^2/s
-absorbance_tuple = ClimaLSM.Canopy.compute_absorbances.(
-    Ref(radiative_transfer),
-    PAR / (energy_per_photon_PAR * N_a),
-    NIR / (energy_per_photon_NIR * N_a),
-    LAI,
-    K,
-    soil_α_PAR,
-    soil_α_NIR,
-    θs,
-    frac_diff,
-)
-# It's returned as a Vector of tuples, so unpack that
-APAR = [absorbance_tuple[k].par[1].abs for k in 1:length(absorbance_tuple)]
-ANIR = [absorbance_tuple[k].nir[1].abs  for k in 1:length(absorbance_tuple)]
-TPAR = [absorbance_tuple[k].par[1].trans  for k in 1:length(absorbance_tuple)]
-TNIR = [absorbance_tuple[k].nir[1].trans  for k in 1:length(absorbance_tuple)]
-RPAR = [absorbance_tuple[k].par[1].refl for k in 1:length(absorbance_tuple)]
-RNIR = [absorbance_tuple[k].nir[1].refl for k in 1:length(absorbance_tuple)]
-
-# Compute outgoing SW; net SW of the canopy, and net SW_soil in W/m^2. These are all positive by definition.
-SW_out_model = energy_per_photon_NIR * N_a .* RNIR .+ energy_per_photon_PAR * N_a .* RPAR
-SW_canopy = energy_per_photon_NIR * N_a .* ANIR .+ energy_per_photon_PAR * N_a .* APAR
-SW_soil = energy_per_photon_NIR * N_a .* TNIR .* (1-soil_α_NIR) .+ energy_per_photon_PAR * N_a .* TPAR .* (1-soil_α_PAR)
-
-# Here we make some approximations. This shouldnt be a big deal, though
-# because it affects GPP and SW absorbed, and those are two metrics where
-# we do fairly well.
-# Stomatal conductance. This is inconsistent with T_canopy_airspace, below.
-medlyn_factor =
-    medlyn_term.(g1, T_air, P_air, q_air, Ref(thermo_params))
-# Treat as fixed.
-β = 0.96
-# Evaluate at T_air. This is inconsistent with T_canopy, below, but 
-Rd = @. ClimaLSM.Canopy.dark_respiration(Vcmax25, β, f, ΔHRd, T_air, To, R)
-An =
-    ClimaLSM.Canopy.compute_photosynthesis.(
-        Ref(photosynthesis),
-        T_air,
-        medlyn_factor,
-        APAR,
-        c_co2_air,
-        β,
-        R,
-        Rd,
-    )
-
-GPP = @. ClimaLSM.Canopy.compute_GPP(An, K, LAI, Ω)
-gs = @. ClimaLSM.Canopy.medlyn_conductance(g0, Drel, medlyn_factor, An, c_co2_air)
-g_canopy = @. ClimaLSM.Canopy.upscale_leaf_conductance(gs, LAI, T_air, R, P_air)
-r_canopy = 1 ./ g_canopy
-ψ_soil = @. ClimaLSM.Soil.matric_potential(hcm, (SWC - θ_r)/(0.52-θ_r))
-r_soil = ClimaLSM.Soil.soil_resistance.(SWC, SWC, 0.0, soil_params)
-ϵ_soil = 0.96
-
-ts_in = @. Thermodynamics.PhaseEquil_pTq(thermo_params, P_air, T_air, q_air)
-ρ_air = @. Thermodynamics.air_density(thermo_params, ts_in)
-cp_m = @. Thermodynamics.cp_m(thermo_params, ts_in)
-Rm_int = @. Thermodynamics.gas_constant_air(thermo_params, ts_in)
-# Extrapolate to the surface to get the density of the air right above the ground.
-ρ_soil = @. ρ_air *
-        (T_soil / T_air)^(Thermodynamics.cv_m(thermo_params, ts_in) / Rm_int)
-q_sat_soil = @. Thermodynamics.q_vap_saturation_generic(
-    thermo_params,
-    T_soil,
-    ρ_soil,
-    Thermodynamics.Liquid(),
-)
-q_soil= @. (q_sat_soil * exp(grav * ψ_soil * M_w / (R * T_soil))) # Bonan's book
-
-
+include(joinpath(climalsm_dir, "experiments/integrated/ozark/set_up_timeseries.jl"))
 
 # Variables we will save
 T_canopy_model = []
@@ -274,10 +90,10 @@ for step in steps
             Thermodynamics.Liquid(),
             )
     ts_canopy = Thermodynamics.PhaseEquil_ρTq(thermo_params, ρ_air[step], T_canopy, q_canopy)
-    canopy_fluxes =  atmos_sfc_fluxes(ts_canopy, ts_in[step], h, d_sfc, u_air[step],z_0m, z_0b, surface_flux_params, thermo_params, r_canopy[step])
+    canopy_fluxes =  atmos_sfc_fluxes(ts_canopy, ts_in[step], h, d_sfc, u_air[step],z_0m, z_0b, surface_flux_params, thermo_params, r_canopy[step],cp_m[step])
     lw_fluxes = land_lw_fluxes(LW_d[step], T_canopy, T_soil[step], ϵ_soil, ϵ_canopy, _σ)
     ts_soil =  Thermodynamics.PhaseEquil_ρTq(thermo_params, ρ_air[step], T_soil[step], q_soil[step])
-    soil_fluxes = atmos_sfc_fluxes(ts_soil, ts_in[step], h, 0.0, u_air[step],0.01, 0.001, surface_flux_params, thermo_params, r_soil[step])
+    soil_fluxes = atmos_sfc_fluxes(ts_soil, ts_in[step], h, 0.0, u_air[step],0.01, 0.001, surface_flux_params, thermo_params, r_soil[step], cp_m[step])
 
     # Save output
     push!(soil_shf, soil_fluxes.shf)
@@ -289,42 +105,92 @@ for step in steps
     push!(LW_soil, lw_fluxes.LW_soil)
               
 end
-
-
+        
 # Plotting
 idx_end = length(soil_shf)
 days = seconds[steps[1:idx_end]] ./ 24 ./ 3600
+#Model - We do not expect conservation because T_canopy set to T_air, but the fluxes dont balance
+# That is, R_n(canopy) ≠ LH(canopy)
+G_model = LW_soil .+ SW_soil[steps[1:idx_end]] .- soil_shf .- soil_lhf;
+net_R_model = @.( LW_d[steps[1:idx_end]] .- LW_out_model) + (SW_d -SW_out_model)[steps[1:idx_end]];
+RminusG_model = net_R_model .- G_model;
+HplusL_model = soil_shf .+ canopy_lhf .+ soil_lhf;
 
-# Weird spikes but not wrong immediately. due to MO solve?
-plt_temp = Plots.plot(days, T_canopy_model, label = "Canopy", title = "Temperature")
-Plots.plot!(plt_temp, days, T_air[steps[1:idx_end]], label = "Atmos")
-Plots.plot!(plt_temp, days, T_soil[steps[1:idx_end]], label = "Soil")
+#Data
+net_R = @. (LW_d-LW_OUT) + (SW_d - SW_OUT);
+RminusG = net_R .- G;
+HplusL = LE .+ H;
 
-# Weird spikes but not wrong immediately
-plt_lw = Plots.plot(days, LW_out_model, label = "Model", title = "LW out")
-Plots.plot!(plt_lw, days, LW_OUT[steps[1:idx_end]], label = "Data")
-
-plt_sw = Plots.plot(days, SW_out_model[steps[1:idx_end]], label = "Model", title = "SW out")
-Plots.plot!(plt_sw, days, SW_OUT[steps[1:idx_end]], label = "Data")
-
-# G still looks bad
-# For us, G is positive = Loss of heat from the ground -> add a sign to data
-plot_G = Plots.plot(days, soil_shf, label = "Soil SHF", title = "Ground heat flux")
-Plots.plot!(plot_G, days, -1 .* LW_soil .- SW_soil[steps[1:idx_end]] .+ soil_shf .+ soil_lhf, label = "Net soil heat flux")
-Plots.plot!(plot_G, days, -1 .* G[steps[1:idx_end]], label = "Data")
-
-# Looks wrong all around
-plot_shf = Plots.plot(days, soil_shf, label  = "Soil-Atmos", title = "SHF")
-Plots.plot!(plot_shf, days, canopy_shf, label  = "Canopy-Atmos")
-Plots.plot!(plot_shf, days, H[steps[1:idx_end]], label = "data",)
-
-# Looks pretty good
-plot_lhf = Plots.plot(days, soil_lhf, label  = "Soil-Atmos", title = "LHF")
-Plots.plot!(plot_lhf, days, canopy_lhf, label  = "Canopy-Atmos")
-Plots.plot!(plot_lhf, days, LE[steps[1:idx_end]], label = "data")
+#=
+# model balance
+Plots.scatter(RminusG_model, HplusL_model, label = "Model", xlabel = "R-G", ylabel = "H+L", title= "Flux Conservation")
+(intercept, slope) = CurveFit.linear_fit(RminusG_model, HplusL_model)
+x = Array(minimum(RminusG_model):10:maximum(RminusG_model))
+y = x .* slope .+intercept
+Plots.plot!(x, y, label = "Fit; slope = $slope")
 
 # Energy balance at the flux tower site:
-recomputed_G = @. (-LE -H + (LW_d-LW_OUT) + (SW_d - SW_OUT))
-RplusG = @. (LW_d-LW_OUT) + (SW_d - SW_OUT) + G
-HplusL = H .+ L
-Plots.scatter(RplusG, HplusL)
+Plots.scatter(RminusG, HplusL, label = "Data", xlabel = "R-G", ylabel = "H+L", title= "Flux Conservation")
+(intercept, slope) = CurveFit.linear_fit(RminusG, HplusL)
+x = Array(minimum(RminusG):10:maximum(RminusG))
+y = x .* slope .+intercept
+Plots.plot!(x, y, label = "Fit; slope = $slope")
+=#
+# Compare net rad
+plt_scatter = Plots.scatter(net_R_model, net_R[steps[1:idx_end]], xlabel = "Model", ylabel = "Data", title= "Net Radiation")
+(intercept, slope) = CurveFit.linear_fit(net_R_model, net_R[steps[1:idx_end]])
+x = Array(minimum(net_R_model):10:maximum(net_R_model))
+y = x .* slope .+intercept
+Plots.plot!(plt_scatter, x, y, label = "Fit; slope = $slope")
+
+# Not perfect but no immediately wrong
+plt_lw = Plots.plot(0.5:0.5:24, diurnal_avg(LW_out_model, 48), label = "Model", title = "LW out", xlabel = "Hour of day")
+Plots.plot!(plt_lw, 0.5:0.5:24, diurnal_avg(LW_OUT[steps[1:idx_end]], 48), label = "Data")
+
+plt_sw = Plots.plot(0.5:0.5:24, diurnal_avg(SW_out_model[steps[1:idx_end]], 48), label = "Model", title = "SW out", xlabel = "Hour of day")
+Plots.plot!(plt_sw, 0.5:0.5:24, diurnal_avg(SW_OUT[steps[1:idx_end]], 48), label = "Data")
+
+Plots.plot(plt_scatter, plt_lw, plt_sw, layout = (1,3))
+savefig("parallel_fluxes_radiation.png")
+
+
+# Compare LHF - not insane
+lhf = canopy_lhf .+ soil_lhf
+plt_scatter = Plots.scatter(lhf, LE[steps[1:idx_end]], xlabel = "Model", ylabel = "Data", title= "LHF")
+(intercept, slope) = CurveFit.linear_fit(lhf, LE[steps[1:idx_end]])
+x = Array(minimum(lhf):10:maximum(lhf))
+y = x .* slope .+intercept
+Plots.plot!(plt_scatter, x, y, label = "Fit; slope = $slope")
+
+plot_lhf = Plots.plot(0.5:0.5:24, diurnal_avg(soil_lhf, 48), label  = "Soil-Atmos", title = "LHF", xlabel = "Hour of day")
+Plots.plot!(plot_lhf, 0.5:0.5:24, diurnal_avg(canopy_lhf, 48), label  = "Canopy-Atmos")
+Plots.plot!(plot_lhf, 0.5:0.5:24, diurnal_avg(canopy_lhf .+ soil_lhf, 48), label  = "Land-Atmos")
+Plots.plot!(plot_lhf, 0.5:0.5:24, diurnal_avg(LE[steps[1:idx_end]], 48), label = "data")
+Plots.plot(plt_scatter, plot_lhf, layout =(1,2))
+savefig("parallel_fluxes_lhf.png")
+
+# G
+plt_scatter = Plots.scatter(G_model, G[steps[1:idx_end]], xlabel = "Model", ylabel = "Data", title= "G")
+(intercept, slope) = CurveFit.linear_fit(G_model, G[steps[1:idx_end]])
+x = Array(minimum(G_model):10:maximum(G_model))
+y = x .* slope .+intercept
+Plots.plot!(plt_scatter, x, y, label = "Fit; slope = $slope")
+
+plot_G = Plots.plot(0.5:0.5:24, diurnal_avg(G_model, 48), label = "Net soil heat flux", title = "G", xlabel = "Hour of day")
+Plots.plot!(plot_G, 0.5:0.5:24, diurnal_avg(G[steps[1:idx_end]], 48), label = "Data")
+Plots.plot(plt_scatter, plot_G, layout =(1,2))
+savefig("parallel_fluxes_G.png")
+
+
+# Looks wrong all around
+shf = soil_shf
+plt_scatter = Plots.scatter(shf, H[steps[1:idx_end]], xlabel = "Model", ylabel = "Data", title= "SHF")
+(intercept, slope) = CurveFit.linear_fit(shf, H[steps[1:idx_end]])
+x = Array(minimum(shf):10:maximum(shf))
+y = x .* slope .+intercept
+Plots.plot!(plt_scatter, x, y, label = "Fit; slope = $slope")
+
+plot_shf = Plots.plot(0.5:0.5:24, diurnal_avg(soil_shf, 48), label  = "Soil-Atmos", title = "SHF", xlabel = "Hour of day")
+Plots.plot!(plot_shf, 0.5:0.5:24, diurnal_avg(H[steps[1:idx_end]], 48), label = "data",)
+Plots.plot(plt_scatter, plot_shf, layout =(1,2))
+savefig("parallel_fluxes_H.png")
