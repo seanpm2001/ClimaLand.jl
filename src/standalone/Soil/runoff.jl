a7666ba58e6059173fb94c23b18e494897f3d8a1
kmdeck <kdeck@caltech.edu>
2023-11-20 15:20:52 -0800

    wip

--- src/standalone/Soil/runoff.jl
+++ src/standalone/Soil/runoff.jl
@@ -64,3 +64,73 @@ append_source(src::AbstractSoilSource, srcs::Tuple)::Tuple = (srcs..., src)
 Appends `src` to the tuple of sources `srcs` if `src` is of type `AbstractSoilSource`.
 """
 append_source(src::Nothing, srcs::Tuple)::Tuple = srcs
+
+
+# Surface runoff at a point
+
+"""
+    PointSurfaceRunoff <: AbstractRunoffModel
+
+A concrete type of soil runoff model, which only
+models surface runoff at a point based on the soil
+surface moisture.
+"""
+struct PointSurfaceRunoff <: AbstractRunoffModel end
+
+"""
+    soil_surface_infiltration(::PointSurfaceRunoff, net_water_flux, Y, p, _)
+
+A function which computes the infiltration into the soil
+in the case of `SF`.
+
+If `net_water_flux = P+E`, where `P` is the precipitation and `E`
+is the evaporation (both negative if towards the soil), 
+this returns `P+E` as the water boundary flux for the soil.
+"""
+function soil_surface_infiltration(::PointSurfaceRunoff, net_water_flux, Y, p, _) where {FT <: AbstractFloat}
+    i_c = soil_infiltration_capacity(Y, p) # field
+    function infl_at_point(i_c, F)
+        if i_c < 0
+            # if i_c < 0 and smaller magnitude than net_water_flux<0, this returns i_c. Runoff = i_c - F
+            # if i_c < 0 and larger magnitude than net_water_flux<0, this returns net_water_flux. Runoff = 0
+            # if i_c < 0 and net_water_flux > 0, this returns net_water_flux. Runoff = 0
+            return max(i_c,F)
+        else
+            if F < 0
+                #if i_c > 0 and net_water_flux < 0, this returns i_c. Runoff = i_c - F
+                return i_c
+            else
+                #if i_c >0 and net_water_flux > 0, this should return the sum. Runoff = i_c
+                return i_c+F
+            end
+        end
+    end
+    return infl_at_point.(i_c, net_water_flux)
+    #in all cases, runoff = boundary_flux - F
+end
+
+        
+
+
+"""
+    function soil_infiltration_capacity(
+        Y::ClimaCore.Fields.FieldVector,
+        p::NamedTuple,
+    )
+
+Computes the infiltration capacity of the soil as
+-K_sfc [ψ(ν) + δz- ψ(θ_sfc)]/δz. 
+
+Note that if θ_sfc > ν,
+the infiltration capacity is positive.
+"""
+function soil_infiltration_capacity(Y::ClimaCore.Fields.FieldVector, p::NamedTuple)
+    FT = eltype(Y.soil.ϑ_l)
+    z = ClimaCore.Fields.coordinate_field(axes(Y.soil.ϑ_l)).z
+    Δz, _ = ClimaLSM.get_Δz(z)
+    p_len = Spaces.nlevels(axes(p.soil.K))
+    K_c = Fields.level(p.soil.K, p_len)
+    ψ_c = Fields.level(p.soil.ψ, p_len)
+    ψ_bc = FT(0)
+    return ClimaCore.Fields.Field(ClimaLSM.diffusive_flux(K_c, ψ_bc .+ Δz, ψ_c, Δz), axes(Δz))
+end
